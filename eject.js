3e7d8bdbb605070d341a58d42f0d06a4ecfdb250
Phil Mayer <phil.mayer93@gmail.com>
2019-09-24 21:01:10 -0400

    feat: (wip) add eject script

---
+++ eject.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+let shell = require('shelljs')
+let colors = require('colors')
+let fs = require('fs') //fs already comes included with node.
+
+// Simple Node.js script to eject all boilerplate repository assets.
+run();
+
+async function run() {
+
+}
