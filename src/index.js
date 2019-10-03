e940bbfe9be9804e1eef337ff12c39f7bd6d9643
Phil Mayer <phil@labelinteractive.com>
2019-10-03 18:02:24 -0400

    chore: add eslint support

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
-
 asyncOne();
 testSpread();
 
+/**
+ * @description Kick off a quick test to see if async/await is working correctly.
+ */
 async function asyncOne() {
     const result = await asyncTwo();
-    console.log(result);
+
+    const testResultNode = document.querySelector('#async-await-result');
+    if (result === 'OK' && testResultNode) {
+        testResultNode.innerText = result;
+        testResultNode.classList.add('test-success');
+    }
 }
 
+/**
+ * @description After a brief delay, resolve the returned promise with a simple message.
+ * @returns {Promise}
+ */
 async function asyncTwo() {
     return new Promise(resolve => {
         setTimeout(() => {
-            resolve('Async await test.');
-        }, 2000);
+            resolve('OK');
+        }, 1000);
     });
 }
 
+/**
+ * @description Quick test to see that some ES6 features are in place, specifically ES6 sets and the spread operator.
+ */
 function testSpread() {
-    const integers = [1, 2, 3, 4];
-    console.log(...integers);
+    const integers = [1, 2, 3, 4, 5, 5];
+    const set = new Set(integers);
+
+    const uniqueValues = [ ...set ];
+    const testResultNode = document.querySelector('#spread-result');
+    if (uniqueValues.length === 5 && testResultNode) {
+        testResultNode.innerText = 'OK';
+        testResultNode.classList.add('test-success');
+    }
 }
