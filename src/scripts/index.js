3d61e98a6a1bd793e6b15ffaabcfdc45b493c6f5
Phil Mayer <phil.mayer93@gmail.com>
2021-03-04 20:52:10 -0500

    chore: format code and bump deps

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,13 +5,13 @@ testSpread();
  * @description Kick off a quick test to see if async/await is working correctly.
  */
 async function asyncOne() {
-    const result = await asyncTwo();
+  const result = await asyncTwo();
 
-    const testResultNode = document.querySelector('#async-await-result');
-    if (result === 'OK' && testResultNode) {
-        testResultNode.innerText = result;
-        testResultNode.classList.add('test-success');
-    }
+  const testResultNode = document.querySelector('#async-await-result');
+  if (result === 'OK' && testResultNode) {
+    testResultNode.innerText = result;
+    testResultNode.classList.add('test-success');
+  }
 }
 
 /**
@@ -19,24 +19,26 @@ async function asyncOne() {
  * @returns {Promise}
  */
 async function asyncTwo() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('OK');
-        }, 1000);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('OK');
+    }, 1000);
+  });
 }
+
+console.log(111);
 
 /**
  * @description Quick test to see that some ES6 features are in place, specifically ES6 sets and the spread operator.
  */
 function testSpread() {
-    const integers = [1, 2, 3, 4, 5, 5];
-    const set = new Set(integers);
+  const integers = [1, 2, 3, 4, 5, 5];
+  const set = new Set(integers);
 
-    const uniqueValues = [...set];
-    const testResultNode = document.querySelector('#spread-result');
-    if (uniqueValues.length === 5 && testResultNode) {
-        testResultNode.innerText = 'OK';
-        testResultNode.classList.add('test-success');
-    }
+  const uniqueValues = [...set];
+  const testResultNode = document.querySelector('#spread-result');
+  if (uniqueValues.length === 5 && testResultNode) {
+    testResultNode.innerText = 'OK';
+    testResultNode.classList.add('test-success');
+  }
 }
